3a0ba84730399c74075288df5849d805f10d1c0a
Jose R. Borja <chakaitos@gmail.com>
2014-03-24 19:56:12 -0500

    Add a get and post routes both matching the same path

--- server.rb
+++ server.rb
@@ -7,3 +7,18 @@ set :bind, '0.0.0.0' # for Vagrant
 get '/welcome/:name' do
   erb :welcome
 end
+
+get '/test' do
+  erb :test
+end
+
+post '/test' do
+  @name = params[:name]
+  @email = params[:mail]
+  @vegetarian = params[:vegetarian]
+  @blood_type = params[:bloodtype]
+  erb :test_summary
+end
+
+
+
